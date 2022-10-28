e064ecd4c3da9fcbbc2e62a198f9811abe787069
JaviAkaSino <javiakasino@hotmail.com>
2022-10-28 13:46:37 +0200

    x y -

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 class Square extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '-',
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
       </button>
     );
   }
