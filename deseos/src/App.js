27fd661354303870ea6de5945ad809ad46e3504d
JaviAkaSino <javiakasino@hotmail.com>
2023-01-11 10:28:04 +0100

    intento

--- deseos/src/App.js
+++ deseos/src/App.js
@@ -4,36 +4,35 @@ import { Button, Form, InputGroup, Input, Row, Col, List } from 'reactstrap';
 import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Desirelist extends Component {
-  render() {
-    return (
-      <List type="unstyled">{this.props.deseos.map(d => <>
-        <PrintDeseo deseo={d} />
-        <Borrar deseo={d} quitar={(elemento) => this.props.quitar(elemento)} />
-      </>)}
-      </List>
-    );
-  }
+function Desirelist() {
+  return (
+    <List type="unstyled">{this.deseos.map(d => <>
+      <PrintDeseo deseo={d} />
+      <Borrar deseo={d} quitar={(elemento) => this.props.quitar(elemento)} />
+    </>)}
+    </List>
+  );
+
 }
 
-class Desire extends Component {
-  render() {
-    return (
-      <Form onSubmit={this.props.onAddDeseo}>
-        <Row xs="3">
-          <Col></Col>
-          <Col>
-            <InputGroup>
-              <Input placeholder="Escribe tu deseo" name="deseo" />
-              <Button color="secondary">Guardar</Button>
-            </InputGroup>
-          </Col>
-          <Col></Col>
-        </Row>
+function Desire() {
 
-      </Form>
-    );
-  }
+  return (
+    <Form onSubmit={this.onAddDeseo}>
+      <Row xs="3">
+        <Col></Col>
+        <Col>
+          <InputGroup>
+            <Input placeholder="Escribe tu deseo" name="deseo" />
+            <Button color="secondary">Guardar</Button>
+          </InputGroup>
+        </Col>
+        <Col></Col>
+      </Row>
+
+    </Form>
+  );
+
 }
 
 function PrintDeseo(props) {
@@ -47,40 +46,37 @@ function Borrar(props) {
     </Button>)
 }
 
-class App extends Component {
+function App() {
   //Centralizamos deseos en componente principal
-  constructor(props) {
-    super(props);
-    this.state = {
-      deseos: ["GAMBAS", "JAMÓN"],
-    }
-  }
+  
+      const deseos= ["GAMBAS", "JAMÓN"];
+    
+  
 
   //Función que añade los deseos a deseos
-  handleAniadirDeseo(event) { //Es como un manejador de eventos
+  function handleAniadirDeseo(event) { //Es como un manejador de eventos
     event.preventDefault(); //Hace que no refresque la página
     //console.log(event.target.deseo.value);
-    let d = this.state.deseos;
+    let d = deseos;
     d.push(event.target.deseo.value);
-    this.setState({ deseos: d });
+    deseos = d;
   }
 
-  quitar(elemento) {
-    let d = this.state.deseos.filter(d => d != elemento);
-    this.setState({ deseos: d });
+  function quitar(elemento) {
+    let d = deseos.filter(d => d != elemento);
+    deseos = d;
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>AÑADE TU DESEO</h1>
-        <Desirelist deseos={this.state.deseos} quitar={(elemento) => this.quitar(elemento)} />
-        <Desire onAddDeseo={this.handleAniadirDeseo.bind(this)} />
-      </div>
-    );
-
-  }
+  return (
+    <div className="App">
+      <h1>AÑADE TU DESEO</h1>
+      <Desirelist deseos={deseos} quitar={(elemento) => quitar(elemento)} />
+      <Desire onAddDeseo={handleAniadirDeseo.bind(this)} />
+    </div>
+  );
 
 }
+
+
 
 export default App;
